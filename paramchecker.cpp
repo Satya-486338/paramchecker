c09718b5d40987615b634b00dc21909e1ff539f2
Satyendra Tiwari <satyendratiwari1991@gmail.com>
2020-07-27 17:04:56 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,7 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   bool boVitalArea = true;
-  if(bpm < 70 || bpm > 150) {
-    boVitalArea = false;
-  } else if(spo2 < 80) {
-    boVitalArea = false;
-  } else if(respRate < 30 || respRate > 60) {
+  if((bpm < 70 || bpm > 150) || (spo2 < 80) || (respRate < 30 || respRate > 60)) {
     boVitalArea = false;
   }
   return boVitalArea;
