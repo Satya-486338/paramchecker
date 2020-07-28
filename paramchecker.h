f41ef885caae29f70d9f753913e42152424737e9
Satyendra Tiwari <satyendratiwari1991@gmail.com>
2020-07-28 15:17:41 +0530

    Update paramchecker.h

--- paramchecker.h
+++ paramchecker.h
@@ -1,5 +1,5 @@
-bool vitalsBpmRateAreOk(float bpm);
-bool vitalsSpo2RateAreOk(float spo2);
-bool vitalsRespRateAreOk(float respRate);
+bool IsboBpmRateAreOk(float bpm);
+bool IsSpo2RateAreOk(float spo2);
+bool ISRespRateAreOk(float respRate);
 bool vitalsRespAreOk(float bpm,float spo2,float respRate);
 
